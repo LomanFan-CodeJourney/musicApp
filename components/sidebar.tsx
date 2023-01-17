871709ee94a1b3f338324daa050b00190b95f003
LomanFan-CodeJourney <lfan.codejourney@gmail.com>
2023-01-16 16:27:17 -0800

    add logo

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,5 +1,39 @@
+import NextImage from 'next/image';
+import {
+  Box,
+  List,
+  ListItem,
+  ListIcon,
+  Divider,
+  Center,
+  LinkBox,
+  LinkOverlay,
+} from '@chakra-ui/layout';
+
+import {
+  MdHome,
+  MdSearch,
+  MdLibraryMusic,
+  MdPlaylistAdd,
+  MdFavorite,
+} from 'react-icons';
+
 const Sidebar = () => {
-  return <div>This is a beautiful sidebar.</div>;
+  return (
+    <Box
+      width="100%"
+      height="calc(100vh - 100px)"
+      bg="black"
+      paddingX="5px"
+      color="gray"
+    >
+      <Box paddingY="20px">
+        <Box width="195px" marginBottom="20px" paddingX="20px">
+          <NextImage src="/moosic-monk-logo.svg" height={60} width={150} />
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Sidebar;
