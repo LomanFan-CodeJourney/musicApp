ad3105c15ffe960c62bbec92728d6e017addc38d
LomanFan-CodeJourney <lfan.codejourney@gmail.com>
2023-01-31 17:37:36 -0800

    add UI layout, gradient background, profile image, title, subtitle etc.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,13 @@
 /* eslint-disable prettier/prettier */
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+// import Head from 'next/head';
+// import Image from 'next/image';
+// import styles from '../styles/Home.module.css';
+import GradientLayout from '../components/gradientLayout';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (<GradientLayout roundImage color="green" subtitle="profile" title="Loman Fan" description="15 public playlists" image="/profile-picture.jpg">
+    <div>Homepage</div>
+  </GradientLayout>)
 };
 
 export default Home;
