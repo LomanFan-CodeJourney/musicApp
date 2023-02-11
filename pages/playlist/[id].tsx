666a0ac0a950a5eeeec5972703e779063b7494b3
LomanFan-CodeJourney <lfan.codejourney@gmail.com>
2023-02-10 21:46:23 -0800

    update SongsTable UI

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -1,6 +1,7 @@
 import { validateToken } from "../../lib/auth";
 import prisma from "../../lib/prisma";
 import GradientLayout from "../../components/gradientLayout";
+import SongTable from '../../components/songsTable';
 
 const getBGColor = (id) => {
   const colors = [
@@ -27,7 +28,7 @@ const Playlist = ({ playlist }) => {
       description={`${playlist.songs.length} songs`}
       image={`https://picsum.photos/400?random=${playlist.id}`}
     >
-      <div>hi</div>
+      <SongTable/>
     </GradientLayout>
   );
 };
