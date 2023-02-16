cb2c92936952bddeecbfe84773c1a2efc3c95b05
LomanFan-CodeJourney <lfan.codejourney@gmail.com>
2023-02-16 14:34:09 -0800

    add Player to PlayerBar

---
+++ components/player.tsx
@@ -0,0 +1,31 @@
+import {
+  ButtonGroup,
+  Box,
+  IconButton,
+  RangeSlider,
+  RangeSliderFilledTrack,
+  RangeSliderTrack,
+  RangSliderThumb,
+  Center,
+  Flex,
+  Text,
+} from '@chakra-ui/react';
+import ReactHowler from 'react-howler';
+import { useEffect, useRef, useState } from 'react';
+import {
+  MdShuffle,
+  MdSkipPrevious,
+  MdSkipNext,
+  MdOutlinePlayCircleFilled,
+  MdOutlinePauseCircleFilled,
+  MdOutlineRepeat,
+} from 'react-icons/md';
+import {useStoreActions} from 'easy-peasy';
+
+const Player = () => {
+  return (
+    <Box>Player</Box>
+  )
+}
+
+export default Player;
