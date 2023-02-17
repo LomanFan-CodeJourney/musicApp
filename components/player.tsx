8d4db0971cd8f6227e4e442d5f3a3f2f1373f004
LomanFan-CodeJourney <lfan.codejourney@gmail.com>
2023-02-16 16:13:58 -0800

    add Player Controls and Seek Bar UI3

--- components/player.tsx
+++ components/player.tsx
@@ -5,7 +5,7 @@ import {
   RangeSlider,
   RangeSliderFilledTrack,
   RangeSliderTrack,
-  RangSliderThumb,
+  RangeSliderThumb,
   Center,
   Flex,
   Text,
@@ -20,12 +20,86 @@ import {
   MdOutlinePauseCircleFilled,
   MdOutlineRepeat,
 } from 'react-icons/md';
-import {useStoreActions} from 'easy-peasy';
+import { useStoreActions } from 'easy-peasy';
 
 const Player = () => {
   return (
-    <Box>Player</Box>
-  )
-}
+    <Box>
+      <Box>{/* <ReactHowler /> */}</Box>
+      <Center color="grey.600">
+        <ButtonGroup>
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="shuffle"
+            fontSize="24px"
+            icon={<MdShuffle />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="skip"
+            fontSize="24px"
+            icon={<MdSkipPrevious />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="play"
+            fontSize="40px"
+            color="white"
+            icon={<MdOutlinePlayCircleFilled />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="pause"
+            fontSize="40px"
+            color="white"
+            icon={<MdOutlinePauseCircleFilled />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="next"
+            fontSize="24px"
+            icon={<MdSkipNext />}
+          />
+          <IconButton
+            outline="none"
+            variant="link"
+            aria-label="repeat"
+            fontSize="24px"
+            icon={<MdOutlineRepeat />}
+          />
+        </ButtonGroup>
+      </Center>
+      <Box color="gray.600">
+        <Flex justify="center" align="center">
+          <Box width="10%">
+            <Text fontSize="xs">1:21</Text>
+          </Box>
+          <Box width="80%">
+            <RangeSlider
+              aria-label={('min', 'max')}
+              step={0.1}
+              min={0}
+              max={321}
+              id="player-range"
+            >
+              <RangeSliderTrack bg="gray.800">
+                <RangeSliderFilledTrack bg="gray.600" />
+              </RangeSliderTrack>
+              <RangeSliderThumb index={0} />
+            </RangeSlider>
+          </Box>
+          <Box width="10%" textAlign="right">
+            <Text fontSize="xs">3:21</Text>
+          </Box>
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
 
 export default Player;
